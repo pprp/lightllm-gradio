94371a5020be4d0f76264680f6cac28114d2b365
pprp <dongpeijie98@gmail.com>
2023-12-08 17:08:00 +0800

    update one gpu

--- run_llama_api_server.sh
+++ run_llama_api_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=3,4,5,6 python -m lightllm.server.api_server \
+CUDA_VISIBLE_DEVICES=3 python -m lightllm.server.api_server \
                             --model_dir /data2/dongpeijie/workspace/lightllm/finetune_chatbotv4 \
                             --host 0.0.0.0                 \
                             --port 8080                    \
-                            --tp 4                     \
+                            --tp 1                     \
                             --max_total_token_num 12000
